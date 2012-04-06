a71a23ba59e10565c15b43971d935b613c58d272
robinzhang <183317744@qq.com>
2012-04-06 14:33:15 +0800

    robin

---
+++ ios/HelloWorld/HelloWorldTests/HelloWorldTests.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldTests.h
+//  HelloWorldTests
+//
+//  Created by robin on 12-4-6.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HelloWorldTests : SenTestCase
+
+@end
---
+++ ios/HelloWorld/HelloWorldTests/HelloWorldTests.m
@@ -0,0 +1,32 @@
+//
+//  HelloWorldTests.m
+//  HelloWorldTests
+//
+//  Created by robin on 12-4-6.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "HelloWorldTests.h"
+
+@implementation HelloWorldTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HelloWorldTests");
+}
+
+@end
